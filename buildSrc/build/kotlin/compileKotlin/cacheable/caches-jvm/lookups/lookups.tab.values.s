bc28a07d20a621deee10e0a337bc7f8d1e81990f
Mikhail Fedotov <MFedotov2@alfabank.ru>
2025-03-04 00:32:25 +0800

    remove buildSrc dir from scm

--- buildSrc/build/kotlin/compileKotlin/cacheable/caches-jvm/lookups/lookups.tab.values.s
+++
@@ -1,16 +0,0 @@
-ÞrõvõvÎfÙSÃTÊTÐTÌ@È2ú2ÆàdûófÆÞˆÁÀuÆà`÷ü`ËÂbÔñ`ðícÒ
-Øj×
-èwÏ
-ÜiË
-Éhý	Ë}à	û}×	Øfä	ÅSä	Û@ì	æ,ÃGâ	Ó×]ú	ô
-ýl÷	Í
-ûnì	Ú
-Èoé
-ü
-×pë
-Þ
-ðpÍ
-ÔÅ›Ê
-î
-Ë—É	Ö
-ÒŒáŽÖüÉ‘Ò“ÝÎŽÁsÖjÆbÀ\ÒcÙbäbþ^ñZáPâOúNçNàUãU
